5196360a461b390e0d650da4fc3f9e26b141a3b2
Miki Mints <mikimn1999@gmail.com>
2021-01-01 16:56:42 +0200

    Different seeds for MNLI fine-tuning

--- src/BERT/scripts/train_mnli_all.sh
+++ src/BERT/scripts/train_mnli_all.sh
@@ -11,34 +11,48 @@ bert_dir="bert-base-uncased"
 
 
 # Train the baseline model.
-#python run_glue.py --cache_dir $cache_dir --do_train  --do_eval \
-#--task_name mnli  --eval_task_names all --model_type bert --num_train_epochs 3 \
-#--lambda_h 0.0  --model_name_or_path $bert_dir --output_dir $outputs_dir/bert_baseline \
-#--overwrite_output_dir  --learning_rate 2e-5 --do_lower_case \
-#--outputfile $results_dir/results.csv  --binerize_eval
-
-# Train the DFL model.
 for seed in "${seeds[@]}"; do
   python run_glue.py \
   --cache_dir $cache_dir \
-  --do_eval \
   --do_train \
+  --do_eval \
   --task_name mnli \
   --eval_task_names all \
   --model_type bert \
   --num_train_epochs 3 \
-  --lambda_h 1.0  \
+  --lambda_h 0.0 \
   --model_name_or_path $bert_dir \
-  --output_dir $outputs_dir/bert_focalloss_seed_"$seed" \
+  --output_dir $outputs_dir/bert_baseline_seed_"$seed" \
   --overwrite_output_dir \
   --learning_rate 2e-5 \
   --do_lower_case \
   --outputfile $results_dir/results.csv \
-  --focal_loss \
-  --seed "$seed" \
-  --nonlinear_h_classifier deep \
-  --binerize_eval
+  --binerize_eval \
+  --seed "$seed"
 done
+
+# Train the DFL model.
+#for seed in "${seeds[@]}"; do
+#  python run_glue.py \
+#  --cache_dir $cache_dir \
+#  --do_eval \
+#  --do_train \
+#  --task_name mnli \
+#  --eval_task_names all \
+#  --model_type bert \
+#  --num_train_epochs 3 \
+#  --lambda_h 1.0  \
+#  --model_name_or_path $bert_dir \
+#  --output_dir $outputs_dir/bert_focalloss_seed_"$seed" \
+#  --overwrite_output_dir \
+#  --learning_rate 2e-5 \
+#  --do_lower_case \
+#  --outputfile $results_dir/results.csv \
+#  --focal_loss \
+#  --seed "$seed" \
+#  --nonlinear_h_classifier deep \
+#  --binerize_eval
+#done
 
 # Train the POE model.
 #python run_glue.py --cache_dir $cache_dir --do_train  --do_eval \
