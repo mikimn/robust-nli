37825af093ddf5f3587d74c774b2c0d251377463
Miki Mints <mikimn1999@gmail.com>
2021-01-03 14:43:26 +0200

    fixed train_hans script

--- src/BERT/scripts/train_hans.sh
+++ src/BERT/scripts/train_hans.sh
@@ -10,36 +10,52 @@ bert_dir="bert-base-uncased"
 # Due to the reported various on this dataset, we report the
 # average results across 4 seeds.
 # Train the baseline model.
-#for seed in ${seeds[@]}; do
-#    python run_glue.py --cache_dir $cache_dir --do_train --do_eval  \
-#    --task_name mnli --eval_task_names   mnli HANS  --model_type bert \
-#    --num_train_epochs 3  --lambda_h 0.0   --model_name_or_path $bert_dir \
-#    --output_dir $outputs_dir/bert_baseline_seed_$seed --overwrite_output_dir \
-#    --learning_rate 2e-5 --do_lower_case  --outputfile $results_dir/results.csv \
-#    --binerize_eval  --seed $seed
-#done
-
-
-# Train the RUBI model.
-#for seed in ${seeds[@]}; do
-#    python run_glue.py --cache_dir $cache_dir --do_train --do_eval  \
-#    --task_name mnli --eval_task_names   mnli HANS  --model_type bert --num_train_epochs 3 \
-#    --lambda_h 1.0   --model_name_or_path $bert_dir --output_dir  $outputs_dir/bert_rubi_seed_$seed \
-#    --overwrite_output_dir --rubi --learning_rate 2e-5 --do_lower_case --outputfile $results_dir/results.csv \
-#    --nonlinear_h_classifier deep  --hans  --similarity mean max min --weighted_bias_only --binerize_eval \
-#    --seed $seed --hans_features
-#done
-
+for seed in "${seeds[@]}"; do
+    python run_glue.py \
+    --cache_dir $cache_dir \
+    --do_train \
+    --do_eval  \
+    --task_name mnli \
+    --eval_task_names mnli HANS \
+    --model_type bert \
+    --num_train_epochs 3 \
+    --lambda_h 0.0 \
+    --model_name_or_path $bert_dir \
+    --output_dir $outputs_dir/bert_baseline_seed_$seed \
+    --overwrite_output_dir \
+    --learning_rate 2e-5 \
+    --do_lower_case \
+    --outputfile $results_dir/results.csv \
+    --binerize_eval \
+    --seed $seed
+done
 
 # Train the DFL model.
-#for seed in ${seeds[@]}; do
-#    python run_glue.py --cache_dir $cache_dir --do_train --do_eval  --task_name mnli \
-#    --eval_task_names  mnli HANS  --model_type bert --num_train_epochs 3  --lambda_h 1.0 \
-#    --model_name_or_path $bert_dir --output_dir $outputs_dir/bert_focalloss_seed_$seed \
-#    --overwrite_output_dir --learning_rate 2e-5 --do_lower_case --outputfile $results_dir/results.csv \
-#    --focal_loss   --nonlinear_h_classifier deep  --hans  --similarity mean max min \
-#    --weighted_bias_only  --binerize_eval  --seed $seed --hans_features
-#done
+for seed in "${seeds[@]}"; do
+    python run_glue.py\
+     --cache_dir $cache_dir \
+     --do_train \
+     --do_eval \
+     --task_name mnli \
+     --eval_task_names mnli HANS \
+     --model_type bert \
+     --num_train_epochs 3 \
+     --lambda_h 1.0 \
+     --model_name_or_path $bert_dir \
+     --output_dir $outputs_dir/bert_focalloss_seed_$seed \
+     --overwrite_output_dir \
+     --learning_rate 2e-5 \
+     --do_lower_case \
+     --outputfile $results_dir/results.csv \
+     --focal_loss \
+     --nonlinear_h_classifier deep \
+     --hans \
+     --similarity mean max min \
+     --weighted_bias_only \
+     --binerize_eval \
+     --seed $seed \
+     --hans_features
+done
 
 
 # Train the POE model.
@@ -68,3 +84,13 @@ for seed in "${seeds[@]}"; do
     --seed "$seed" \
     --hans_features
 done
+
+# Train the RUBI model.
+#for seed in ${seeds[@]}; do
+#    python run_glue.py --cache_dir $cache_dir --do_train --do_eval  \
+#    --task_name mnli --eval_task_names   mnli HANS  --model_type bert --num_train_epochs 3 \
+#    --lambda_h 1.0   --model_name_or_path $bert_dir --output_dir  $outputs_dir/bert_rubi_seed_$seed \
+#    --overwrite_output_dir --rubi --learning_rate 2e-5 --do_lower_case --outputfile $results_dir/results.csv \
+#    --nonlinear_h_classifier deep  --hans  --similarity mean max min --weighted_bias_only --binerize_eval \
+#    --seed $seed --hans_features
+#done
