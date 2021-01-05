ff41829761f745ebd06069b257d8070171f13236
mikimn <mikimn1999@gmail.com>
2021-01-05 19:51:39 +0200

    Refactor training scripts

--- src/BERT/scripts/train_mnli_all.sh
+++ src/BERT/scripts/train_mnli_all.sh
@@ -11,25 +11,25 @@ bert_dir="bert-base-uncased"
 
 
 # Train the baseline model.
-for seed in "${seeds[@]}"; do
-  python run_glue.py \
-  --cache_dir $cache_dir \
-  --do_train \
-  --do_eval \
-  --task_name mnli \
-  --eval_task_names all \
-  --model_type bert \
-  --num_train_epochs 3 \
-  --lambda_h 0.0 \
-  --model_name_or_path $bert_dir \
-  --output_dir $outputs_dir/bert_baseline_seed_"$seed" \
-  --overwrite_output_dir \
-  --learning_rate 2e-5 \
-  --do_lower_case \
-  --outputfile $results_dir/results.csv \
-  --binerize_eval \
-  --seed "$seed"
-done
+#for seed in "${seeds[@]}"; do
+#  python run_glue.py \
+#  --cache_dir $cache_dir \
+#  --do_train \
+#  --do_eval \
+#  --task_name mnli \
+#  --eval_task_names all \
+#  --model_type bert \
+#  --num_train_epochs 3 \
+#  --lambda_h 0.0 \
+#  --model_name_or_path $bert_dir \
+#  --output_dir $outputs_dir/bert_baseline_seed_"$seed" \
+#  --overwrite_output_dir \
+#  --learning_rate 2e-5 \
+#  --do_lower_case \
+#  --outputfile $results_dir/results.csv \
+#  --binerize_eval \
+#  --seed "$seed"
+#done
 
 # Train the DFL model.
 #for seed in "${seeds[@]}"; do
@@ -55,8 +55,23 @@ done
 #done
 
 # Train the POE model.
-#python run_glue.py --cache_dir $cache_dir --do_train  --do_eval \
-#--task_name mnli  --eval_task_names  all --model_type bert --num_train_epochs 3 \
-#--lambda_h 1.0  --model_name_or_path $bert_dir --output_dir $outputs_dir/bert_poeloss \
-#--overwrite_output_dir --learning_rate 2e-5 --do_lower_case --outputfile $results_dir/results.csv \
-#--poe_loss   --nonlinear_h_classifier deep  --binerize_eval
+for seed in "${seeds[@]}"; do
+  python run_glue.py \
+    --cache_dir $cache_dir \
+    --do_train \
+    --do_eval \
+    --task_name mnli \
+    --eval_task_names all \
+    --model_type bert \
+    --num_train_epochs 3 \
+    --lambda_h 1.0 \
+    --model_name_or_path $bert_dir \
+    --output_dir $outputs_dir/bert_poeloss \
+    --overwrite_output_dir \
+    --learning_rate 2e-5 \
+    --do_lower_case \
+    --outputfile $results_dir/results.csv \
+    --poe_loss \
+    --nonlinear_h_classifier deep \
+    --binerize_eval
+done
