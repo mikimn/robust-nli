44775ce9dabdeea4f1fe54fdc20448c675481999
mikimn <mikimn1999@gmail.com>
2021-01-12 20:15:41 +0200

    Add varying gamma-dfl scripts

--- src/BERT/scripts/train_dfl_parameters.sh
+++ src/BERT/scripts/train_dfl_parameters.sh
@@ -10,26 +10,26 @@ bert_dir="bert-base-uncased"
 gammas=(1.0 2.0 3.0 4.0)
 
 # Train the DFL model.
-for gamma in "${gammas[@]}"; do
-  python run_glue.py \
-  --cache_dir $cache_dir  \
-  --do_eval \
-  --do_train \
-  --task_name mnli \
-  --eval_task_names all \
-  --model_type bert \
-  --num_train_epochs 3 \
-  --gamma_focal ${gamma} \
-  --model_name_or_path $bert_dir \
-  --output_dir $outputs_dir/bert_focalloss_mnli_gamma_${gamma} \
-  --overwrite_output_dir \
-  --learning_rate 2e-5 \
-  --do_lower_case \
-  --outputfile $results_dir/results_lambda.csv \
-  --focal_loss \
-  --nonlinear_h_classifier deep \
-  --binerize_eval
-done
+#for gamma in "${gammas[@]}"; do
+#  python run_glue.py \
+#  --cache_dir $cache_dir  \
+#  --do_eval \
+#  --do_train \
+#  --task_name mnli \
+#  --eval_task_names all \
+#  --model_type bert \
+#  --num_train_epochs 3 \
+#  --gamma_focal ${gamma} \
+#  --model_name_or_path $bert_dir \
+#  --output_dir $outputs_dir/bert_focalloss_mnli_gamma_${gamma} \
+#  --overwrite_output_dir \
+#  --learning_rate 2e-5 \
+#  --do_lower_case \
+#  --outputfile $results_dir/results_lambda.csv \
+#  --focal_loss \
+#  --nonlinear_h_classifier deep \
+#  --binerize_eval
+#done
 
 for gamma in "${gammas[@]}"; do
   python run_glue.py \
@@ -53,6 +53,5 @@ for gamma in "${gammas[@]}"; do
   --similarity mean max min \
   --weighted_bias_only \
   --binerize_eval \
-  --seed $seed \
   --hans_features
 done
