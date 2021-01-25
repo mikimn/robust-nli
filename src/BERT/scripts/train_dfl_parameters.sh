de0816e5c5bd3cd3280d233717bac88937230171
mikimn <mikimn1999@gmail.com>
2021-01-25 22:06:54 +0200

    Add py-sbatch for slurm

--- src/BERT/scripts/train_dfl_parameters.sh
+++ src/BERT/scripts/train_dfl_parameters.sh
@@ -7,32 +7,12 @@ mkdir -p $outputs_dir
 # Sets the paths to the downloaded BERT model and the cache dir.
 cache_dir="temp/cache_dir/"
 bert_dir="bert-base-uncased"
-gammas=(1.0 2.0 3.0 4.0)
+# gammas=(1.0 2.0 3.0 4.0)
+gammas=(0.5 1.5 2.5 3.5 6.0)
 
-# Train the DFL model.
-#for gamma in "${gammas[@]}"; do
-#  python run_glue.py \
-#  --cache_dir $cache_dir  \
-#  --do_eval \
-#  --do_train \
-#  --task_name mnli \
-#  --eval_task_names all \
-#  --model_type bert \
-#  --num_train_epochs 3 \
-#  --gamma_focal ${gamma} \
-#  --model_name_or_path $bert_dir \
-#  --output_dir $outputs_dir/bert_focalloss_mnli_gamma_${gamma} \
-#  --overwrite_output_dir \
-#  --learning_rate 2e-5 \
-#  --do_lower_case \
-#  --outputfile $results_dir/results_lambda.csv \
-#  --focal_loss \
-#  --nonlinear_h_classifier deep \
-#  --binerize_eval
-#done
-
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 for gamma in "${gammas[@]}"; do
-  python run_glue.py \
+  $SCRIPTPATH/py-sbatch.sh run_glue.py \
   --cache_dir $cache_dir \
   --do_train \
   --do_eval \
